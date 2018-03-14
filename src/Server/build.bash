12e02b7ffd98dd40d5dd2063db4bf162cba77d0c
Luke Snyder <ls008@uark.edu>
2018-03-14 14:51:53 -0500

    Unify script.

--- src/Server/build.bash
+++ src/Server/build.bash
@@ -5,6 +5,6 @@ echo Compiling...
 javac -cp ../../lib/java-json.jar *.java
 
 echo Running...
-java -cp .:../../lib/java-json.jar CommandServer
+java -cp ../../lib/java-json.jar:. CommandServer & py ../Client/main.py && fg
 
 
