07645b971333d8fc33e0bb52ddfbb97b9f9587d1
Ian Perkins <irperkin@uark.edu>
2018-04-29 16:59:46 -0500

    adding installer instructions and bat files

---
+++ Installer.bat
@@ -0,0 +1,6 @@
+python -m pip install --upgrade pip
+pip install --upgrade setuptools
+pip install wheels/PyQt4-4.11.4-cp34-cp34m-win_amd64.whl
+pip install opencv-python
+pip install imageio
+pip install moviepy
